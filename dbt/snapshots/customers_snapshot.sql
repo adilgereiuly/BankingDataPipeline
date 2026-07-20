e16aa4f11c76135814eafd603dbcda149d4e3ac2
Orynbassar <o.adilgereuily@gmail.com>
2026-07-21 01:31:46 +0500

    Add customers_snapshot for customer data tracking

--- dbt/snapshots/customers_snapshot.sql
+++ dbt/snapshots/customers_snapshot.sql
@@ -1,1 +1,14 @@
+{% snapshot customers_snapshot %}
 
+{{
+    config(
+        target_schema='silver',
+        unique_key='customer_id',
+        strategy='check',
+        check_cols=['status']
+    )
+}}
+
+SELECT * FROM {{ ref('int_customers_current') }}
+
+{% endsnapshot %}
